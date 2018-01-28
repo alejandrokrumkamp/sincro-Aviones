5010ef805bd958975b43213cfbe68074d1502a71
Alejandro Krumkamp <alekd9@gmail.com>
2018-01-28 14:34:41 -0300

    Logs sincronizados. Se puede configurar la cantidad de aviones

--- main.c
+++ main.c
@@ -20,6 +20,7 @@ sem_t semaforo_pista_liberada;
 
 pthread_mutex_t mutex_pistas;
 
+int cantidadAviones = 100;
 int pistasLibres = 10;
 
 void mantenimiento(){
@@ -27,14 +28,17 @@ void mantenimiento(){
 }
 
 void despegar(){
+	usleep(1000);
 	printf("%i [Avion] Despegue Realizado \n", (int)syscall(__NR_gettid));
 }
 
 void volar(){
+	sleep(1);
 	printf("%i [Avion] Vuelo Realizado \n", (int)syscall(__NR_gettid));
 }
 
 void aterrizar(){
+	usleep(1000);
 	printf("%i [Avion] Aterrizaje Realizado \n", (int)syscall(__NR_gettid));
 }
 
@@ -53,13 +57,13 @@ void * avion(){
 	while(1){
 
 		mantenimiento();
+
 		//pedir una pista y esperar
 		sem_post(&semaforo_pedido);
 		sem_wait(&semaforo_disponible);
 		despegar();
 		sem_post(&semaforo_pista_liberada);
 
-		usleep(2000);
 		volar();
 
 		sem_post(&semaforo_pedido);
@@ -76,14 +80,14 @@ void * controladorEntrada(){
 		sem_wait(&semaforo_pedido);
 		sem_wait(&semaforo_pistas);
 
-		pthread_mutex_lock(&mutex_pistas);
 		otorgarUnaPista();
+
+		pthread_mutex_lock(&mutex_pistas);
 		pistasLibres--;
 		pthread_mutex_unlock(&mutex_pistas);
 
-		sem_post(&semaforo_disponible);
-
 		logger();
+		sem_post(&semaforo_disponible);
 
 	}
 }
@@ -92,15 +96,37 @@ void * controladorSalida(){
 	while(1){
 
 		sem_wait(&semaforo_pista_liberada);
-		pthread_mutex_lock(&mutex_pistas);
+
 		liberarUnaPista();
+
+		pthread_mutex_lock(&mutex_pistas);
 		pistasLibres++;
 		pthread_mutex_unlock(&mutex_pistas);
-		sem_post(&semaforo_pistas);
 
 		logger();
+		sem_post(&semaforo_pistas);
 
 	}
+}
+
+void generarAviones() {
+	int cantidadAvionesCreados = 0;
+	pthread_t hiloAvion;
+	while (cantidadAvionesCreados <= cantidadAviones) {
+		pthread_create(&hiloAvion, NULL, avion, NULL);
+		printf("Cantidad aviones creados: %i|", cantidadAvionesCreados);
+		cantidadAvionesCreados++;
+	}
+}
+
+void generarControladoresAereos() {
+	pthread_t hiloControladorEntrada, hiloControladorSalida;
+	pthread_create(&hiloControladorEntrada, NULL, controladorEntrada, NULL);
+	pthread_create(&hiloControladorSalida, NULL, controladorSalida, NULL);
+
+	// Los joins se agregar para que main no termine.
+	pthread_join(hiloControladorEntrada, NULL);
+	pthread_join(hiloControladorSalida, NULL);
 }
 
 int main(void){
@@ -110,36 +136,8 @@ int main(void){
 	sem_init(&semaforo_pistas, 0, 10);
 	sem_init(&semaforo_pista_liberada, 0, 0);
 
-	pthread_t hiloAvion1,hiloAvion2,hiloAvion3,hiloAvion4,hiloAvion5;
-	pthread_t hiloAvion6,hiloAvion7,hiloAvion8,hiloAvion9,hiloAvion10;
-	pthread_t hiloControladorEntrada,hiloControladorSalida;
 
-	pthread_create(&hiloAvion1, NULL, avion, NULL);
-	pthread_create(&hiloAvion2, NULL, avion, NULL);
-	pthread_create(&hiloAvion3, NULL, avion, NULL);
-	pthread_create(&hiloAvion4, NULL, avion, NULL);
-	pthread_create(&hiloAvion5, NULL, avion, NULL);
-	pthread_create(&hiloAvion6, NULL, avion, NULL);
-	pthread_create(&hiloAvion7, NULL, avion, NULL);
-	pthread_create(&hiloAvion8, NULL, avion, NULL);
-	pthread_create(&hiloAvion9, NULL, avion, NULL);
-	pthread_create(&hiloAvion10, NULL, avion, NULL);
-
-	pthread_create(&hiloControladorEntrada, NULL, controladorEntrada, NULL);
-	pthread_create(&hiloControladorSalida, NULL, controladorSalida, NULL);
-
-	pthread_join(hiloAvion1, NULL);
-	pthread_join(hiloAvion2, NULL);
-	pthread_join(hiloAvion3, NULL);
-	pthread_join(hiloAvion4, NULL);
-	pthread_join(hiloAvion5, NULL);
-	pthread_join(hiloAvion6, NULL);
-	pthread_join(hiloAvion7, NULL);
-	pthread_join(hiloAvion8, NULL);
-	pthread_join(hiloAvion9, NULL);
-	pthread_join(hiloAvion10, NULL);
-	pthread_join(hiloControladorEntrada, NULL);
-	pthread_join(hiloControladorSalida, NULL);
-
+	generarAviones(cantidadAviones);
+	generarControladoresAereos();
 	return 0;
 }
